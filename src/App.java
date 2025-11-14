7705f0e1da5aa7ae5fcdad033774d5b01a647d56
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-11-14 10:34:13 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /**
+         * Given an integre array 'nums', return an array 'answer' such that answer[i]
+         * is equal to the product of all the elements of 'nums' except nums[i].
+         * 
+         * The product of any prefix or suffix of nums is guranteed to fit in a 32-bit
+         * integer
+         * 
+         * You must write an algorithm that runs in O(n) time and without using the
+         * division operator.
+         * 
+         * Input:[1,2,3,4]
+         * Output:[24,12,8,6]
+         * 
+         * Input:[-1,1,0,,-3,3]
+         * Output:[0,0,9,0,0]
+         * 
+         */
+        System.out.println("Hello, World!");
+    }
+}
