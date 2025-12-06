9447abefcf74e8ab251cc9a439c40b41e62ab122
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-12-06 10:29:49 +0100

    Feature: Test commit.

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         /**
-         * Given an integre array 'nums', return an array 'answer' such that answer[i]
+         * Given an integer array 'nums', return an array 'answer' such that answer[i]
          * is equal to the product of all the elements of 'nums' except nums[i].
          * 
          * The product of any prefix or suffix of nums is guranteed to fit in a 32-bit
